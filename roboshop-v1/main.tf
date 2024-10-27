858fa3267ee6cd9b1e1385f89f2608685953ce36
vivekaps16 <vivekaps16@gmail.com>
2024-10-27 15:07:30 +0530

    deploy full inv with tf

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,12 +1,12 @@
-#resource "aws_instance" "frontend" {
-#    ami = "ami-09c813fb71547fc4f"
-#    instance_type = "t3.small"
-#    vpc_security_group_ids= ["sg-07a0852eaf0b31296"]
-#    tags = {
-#      Name = "frontend" 
-#    }
-#  
-#}
+resource "aws_instance" "frontend" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids= ["sg-07a0852eaf0b31296"]
+    tags = {
+      Name = "frontend" 
+    }
+  
+}
 
 resource "aws_instance" "Mmongodb" {
     ami = "ami-09c813fb71547fc4f"
