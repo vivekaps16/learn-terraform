ab65fc747e810322f33a4fd18e2cbe400329d733
vivekaps16 <vivekaps16@gmail.com>
2024-10-27 14:51:08 +0530

    create instance with name security gp id and name

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,5 +1,9 @@
 resource "aws_instance" "frontend" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.small"
+    vpc_security_group_ids= ["sg-07a0852eaf0b31296"]
+    tags = {
+      Name = "frontend" 
+    }
   
 }
