7dfcf00119e185e6553d020f4911b5f9f63d4d8f
vivekaps16 <vivekaps16@gmail.com>
2024-10-28 15:31:48 +0530

    update

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -17,7 +17,7 @@
 
 
 resource "aws_instance" "frontend" {
-  ami = "ami-09c813fb71547fc4f"
+  ami         = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids= ["sg-07a0852eaf0b31296"]
   tags = {
