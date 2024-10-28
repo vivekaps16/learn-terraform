27300a8b4ac95fc5cfd5f9f7c58404034e5b7381
vivekaps16 <vivekaps16@gmail.com>
2024-10-28 15:16:35 +0530

    update

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "frontend-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -45,7 +45,7 @@ resource "aws_instance" "Mmongodb" {
 }
 
 resource "aws_route53_record" "mongodb" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "mongodb-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -63,7 +63,7 @@ resource "aws_instance" "catalogue" {
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "catalogue-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -81,7 +81,7 @@ resource "aws_instance" "redis" {
 }
 
 resource "aws_route53_record" "redis" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "redis-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -99,7 +99,7 @@ resource "aws_instance" "user" {
 }
 
 resource "aws_route53_record" "user" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "user-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -117,7 +117,7 @@ resource "aws_instance" "cart" {
 }
 
 resource "aws_route53_record" "cart" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "cart-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -136,7 +136,7 @@ resource "aws_instance" "mysql" {
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "mysql-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -155,7 +155,7 @@ resource "aws_instance" "shipping" {
 }
 
 resource "aws_route53_record" "shipping" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "shipping-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -174,7 +174,7 @@ resource "aws_instance" "rabbitmq" {
 }
 
 resource "aws_route53_record" "rabbitmq" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "rabbitmq-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -193,7 +193,7 @@ resource "aws_instance" "payment" {
 }
 
 resource "aws_route53_record" "payment" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "payment-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
@@ -212,7 +212,7 @@ resource "aws_instance" "dispatch" {
 }
 
 resource "aws_route53_record" "dispatch" {
-  zone_id = "${Z06362873ENTBVG3OQMG8}"
+  zone_id = "Z06362873ENTBVG3OQMG8"
   name    = "dispatch-dev.devopspractice.info"
   type    = "A"
   ttl     = "30"
