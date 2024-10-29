9509a778dec85b7e112bbd299742036347261b85
vivekaps16 <vivekaps16@gmail.com>
2024-10-29 14:28:40 +0530

    code copy

--- roboshop-data-source/main.tf
+++ roboshop-data-source/main.tf
@@ -1,18 +1,37 @@
-resource "aws_instance" "test" {
-  ami = data.aws_ami.ami.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids= ["data.aws_security_group.sg.id"]
-  tags = {
-    Name = "test" 
-  }
+#resource "aws_instance" "test" {
+#  ami = data.aws_ami.ami.id
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids= ["data.aws_security_group.sg.id"]
+#  tags = {
+#    Name = "test" 
+#  }
+#
+#}
+#
+#data "aws_ami" "ami" {
+#  most_recent      = true
+#  name_regex       = "RHEL-9-DevOps-Practice"
+#  owners           = ["973714476881"]
+#
+#}
+#
+#data "aws_security_group" "sg" {
+#  name = "allow-all"
+#}
 
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags = {
+    Name = "test"
+  }
 }
 
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
-
 }
 
 data "aws_security_group" "sg" {
