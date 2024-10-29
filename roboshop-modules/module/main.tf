bf6f1b23b935381bae0b757ed658c472290e1c1a
vivekaps16 <vivekaps16@gmail.com>
2024-10-29 14:45:13 +0530

    modules

--- roboshop-modules/module/main.tf
+++ roboshop-modules/module/main.tf
@@ -1,6 +1,6 @@
    resource "nul_resource" "test" {}
-   resource "nul_resource" "test" {}
-   resource "nul_resource" "test" {}
-   resource "nul_resource" "test" {}
-   resource "nul_resource" "test" {}
-   resource "nul_resource" "test" {}
+   resource "nul_resource" "test1" {}
+   resource "nul_resource" "test2" {}
+   resource "nul_resource" "test3" {}
+   resource "nul_resource" "test4" {}
+   resource "nul_resource" "test5" {}
