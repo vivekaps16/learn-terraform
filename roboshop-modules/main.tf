f67da14c623c24c6c78836e8ef8c8dd8f529a198
vivekaps16 <vivekaps16@gmail.com>
2024-10-30 11:58:09 +0530

    module

--- roboshop-modules/main.tf
+++ roboshop-modules/main.tf
@@ -1,4 +1,22 @@
-module "test" {
+module "roboshop" {
+  for_each = var.instances
   source = "./module"
-  
+  component_name = each.key  
+}
+
+
+variable "instances" {
+  default = {
+    frontend = {}
+    cart = {}
+    mysql = {}
+    mongodb = {}
+    user = {}
+    redis = {}
+    catalogue = {}
+    payment = {}
+    rabbitmq = {}
+    shipping = {}
+    dispatch = {}
+  }
 }
