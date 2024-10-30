86c6d74b4a56fb8f15c6d5183ba400a57ca5fa9c
vivekaps16 <vivekaps16@gmail.com>
2024-10-30 11:55:09 +0530

    module

--- roboshop-modules/module/main.tf
+++ roboshop-modules/module/main.tf
@@ -1,6 +1,20 @@
-   resource "nul_resource" "test" {}
-   resource "nul_resource" "test1" {}
-   resource "nul_resource" "test2" {}
-   resource "nul_resource" "test3" {}
-   resource "nul_resource" "test4" {}
-   resource "nul_resource" "test5" {}
+resource "aws_instance" "instance" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids= ["sg-07a0852eaf0b31296"]
+  tags = {
+    Name = var.component_name
+  }
+
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z06362873ENTBVG3OQMG8"
+  name    = "${var.component_name}-dev.devopspractice.info"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.instance.private_ip]
+}
+
+
+variable "component_name" {}
