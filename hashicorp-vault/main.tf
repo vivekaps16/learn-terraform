d15ddf706bd227720d1562d851e4381c1250d1fb
vivekaps16 <vivekaps16@gmail.com>
2024-11-19 11:49:13 +0530

    hashi corp terra

--- hashicorp-vault/main.tf
+++ hashicorp-vault/main.tf
@@ -1,18 +1,17 @@
 provider "vault" {
   address = "http://172.31.85.68:8200"
-  token   = var.vault_token
-  skip_tls_verification = true
+  token = var.vault_token
+  skip_tls_verify = true
 }
 
 variable "vault_token" {}
 
-data "vault_kv_secret_v2" "example" {
-  name = "credentials"
-  mount = "test"
+data "vault_generic_secret" "example" {
+  path = "credentials"
+ }
+
+
+resource "local_file" "foo" {
+  content  = data.vault_generic_secret.example.data["password"]
+  filename = "/tmp/secret"  
 }
-
-
-#resource "local_file" "foo" {
-#  content  = data.vault_generic_secret.example.data["password"]
-#  filename = "/tmp/secret"  
-#}
