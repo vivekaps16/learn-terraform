129a9426e7c1dfeb02df8c287263bc9e1a4ff892
vivekaps16 <vivekaps16@gmail.com>
2024-11-19 11:58:18 +0530

    vault

--- hashicorp-vault/main.tf
+++ hashicorp-vault/main.tf
@@ -1,17 +1,25 @@
 provider "vault" {
-  address = "http://54.166.78.232:8200"
-  token = var.vault_token
+  address = "https://172.31.85.68:8200"
+  token   = var.vault_token
   skip_tls_verify = true
 }
 
 variable "vault_token" {}
 
 data "vault_generic_secret" "example" {
-  path = "credentials"
- }
+  path = "test/credentials"
+}
 
+# data "vault_kv_secret_v2" "example" {
+#   name  = "my_credentials"
+#   mount = "test"
+# }
 
 resource "local_file" "foo" {
   content  = data.vault_generic_secret.example.data["password"]
-  filename = "/tmp/secret"  
+  filename = "/tmp/secret"
 }
+
+# output "json" {
+#   value = data.vault_kv_secret_v2.example.data_json
+# }
